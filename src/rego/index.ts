1909e475c4c7b616e04cd55bed5614237cd50c0b
Matthijs Galesloot <mgalesloot@gmail.com>
2025-05-07 09:59:15 +0000

    fix: tsc
    Signed-off-by: Matthijs Galesloot <mgalesloot@gmail.com>

--- src/rego/index.ts
+++ src/rego/index.ts
@@ -7,7 +7,6 @@ export * from './Rule';
 
 export * from './frameworks';
 export * from './controls';
-export * from './rules';
 
 export const defaultFrameworkNames = ['AllControls', 'cis-v1.10.0', 'MITRE', 'NSA'];
 
